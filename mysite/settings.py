d4ceeb9431d079d2ea0d7c326dd0d8a39e3f2825
uzoma chimdindu precious <uzomachimdindu300@gmail.com>
2023-01-04 11:27:40 +0100

    first commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*(mh9()&9*@tstdx#nhu-6d92hl%2q)epg#am5iuo-v7ktr8h)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
